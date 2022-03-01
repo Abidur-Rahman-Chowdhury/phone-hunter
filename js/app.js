7d4f6789addd2a49893805dc6f6763424f879f49
Abidur Rahman <abid43iiuc@gmail.com>
2022-03-01 16:26:51 +0600

    done project

--- js/app.js
+++ js/app.js
@@ -23,6 +23,8 @@ const displayData = (data) => {
 
     showCard.textContent = "";
     searchInput.value = "";
+    const showMoreDetails = document.getElementById("show-more-details");
+    showMoreDetails.textContent = "";
   } else {
     getError.textContent = "";
     const searchInput = document.getElementById("search-btn");
@@ -32,6 +34,8 @@ const displayData = (data) => {
     const showCard = document.getElementById("show-card");
 
     showCard.textContent = "";
+    const showMoreDetails = document.getElementById("show-more-details");
+    showMoreDetails.textContent = "";
 
     for (const result of resultData) {
       const div = document.createElement("div");
@@ -70,25 +74,42 @@ const displayDetails = (data) => {
 
   const showMoreDetails = document.getElementById("show-more-details");
   showMoreDetails.textContent = "";
+  const sensors = [...data.data.mainFeatures.sensors].join(", ");
+  const otherInformation = data.data.others;
+
+  console.log(sensors);
   const div1 = document.createElement("div");
   div1.classList.add("col-5");
   const div2 = document.createElement("div");
-    div2.classList.add("col-7");
+  div2.classList.add("col-7");
+  const div3 = document.createElement("div");
+  if (otherInformation != undefined) {
+    for (const [key, value] of Object.entries(otherInformation)) {
+      const li = document.createElement("li");
+      li.innerText = `${key}: ${value}`;
+      div3.appendChild(li);
+      console.log(`${key}: ${value}`);
+    }
+  } else {
+    div3.innerText = "not found";
+  }
+
   div1.innerHTML = `
       <img src="${data.data.image}" class="img-fluid" /> 
     `;
   div2.innerHTML = `
     <h3 >${data.data.name}</h3>
     <strong> ${data.data?.releaseDate || "Release date not found"}</strong>
-    <h4 class='mt-1 text-center'>Main Features</h4>
+    <h4 class='mt-1'>Main Features:</h4>
     <li>Storage:${data.data.mainFeatures.storage}</li>
     <li>Display Size:${data.data.mainFeatures.displaySize}</li>
     <li>Chip Set:${data.data.mainFeatures.chipSet}</li>
     <li>Memory:${data.data.mainFeatures.memory}</li>
-    <h4>Sensors:</h4>
-     <div id="show-sensor">
-    </div>
+    <h4 >Sensors:</h4>
+    <li>${sensors}</li>
+     <h4>Others:</h4>   
     `;
   showMoreDetails.appendChild(div1);
   showMoreDetails.appendChild(div2);
+  div2.appendChild(div3);
 };
